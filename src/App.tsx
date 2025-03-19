653c7eb52461766dfebe14ca547e9c378308d8e3
Ivozeraa <joseivo2008@gmail.com>
2025-03-19 11:41:13 -0300

    Tailwind and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { Header } from "./components/Header";
-import {Login} from "./pages/login/login";
-import {Register} from "./pages/register/register";
-import S from './App.module.css'
+import S from "./App.module.css";
+import AppRoutes from "./routes";
 
 //depois eu arrumo o Login e o Registro, oto entendendo como funciona
-
 
 function App() {
   return (
     <>
+    <AppRoutes />
       <div className={S.app}>
-      <Header />
-   </div>
+        <Header />
 
-
-      <Login />
-      <Register />
+      </div>
     </>
   );
 }
