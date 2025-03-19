708315e07fa45ee793881c0986e6f9c266471338
Erik3331 <erikramon3331@gmail.com>
2025-03-18 21:37:57 -0300

    Add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
 import { Header } from './components/Header'
+
+import S from './App.module.css'
 
 function App() {
   return (
-   <div>
+   <div className={S.app}>
       <Header />
    </div>
   )
