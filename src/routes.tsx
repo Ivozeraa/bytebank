e1d380a96818e57fa8b117a9c047b05fc95396e7
Ivozeraa <joseivo2008@gmail.com>
2025-03-20 16:10:03 -0300

    Fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/login/login";
 import { DefaultLayout } from "./layouts/defaultLayout";
+import { Register } from "./pages/register/register";
 
 // const PrivateRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
 //   const isAuthenticated = !!localStorage.getItem("token");
@@ -13,6 +14,7 @@ const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/" element={<DefaultLayout />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
       </Routes>
     </Router>
   );
