cfbdaae15a5cc147876d927c9e03f4dd394b77f9
Ivozeraa <joseivo2008@gmail.com>
2025-03-21 14:46:16 -0300

    Fix Error

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,109 +1,64 @@
-import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-import {
-  FaTachometerAlt,
-  FaWallet,
-  FaExchangeAlt,
-  FaCreditCard,
-  FaChartLine,
-  FaHandHoldingUsd,
-  FaCog,
-  FaSignOutAlt,
-} from "react-icons/fa";
+import { FaTachometerAlt, FaWallet, FaExchangeAlt, FaCreditCard, FaChartLine, FaHandHoldingUsd, FaCog, FaSignOutAlt } from 'react-icons/fa';
 
-import S from "./styles/Sidebar.module.css";
+import S from './styles/Sidebar.module.css';
 
 export const Sidebar = () => {
+  const [isLogged, setIsLogged] = useState(false);
   const location = useLocation();
 
+  const handleLogout = () => {
+    setIsLogged(false);
+  };
+
   const getLinkClass = (path: string) => {
-    return location.pathname === path ? S.active : "";
+    return location.pathname === path ? S.active : '';
   };
 
   return (
     <aside className={S.sidebar}>
       <div className={S.mainSidebar}>
-        <Link
-          to="/dashboard"
-          className={`${S.sidebarPage} ${getLinkClass("/dashboard")}`}
-        >
-          <div className={S.icon}>
-            <FaTachometerAlt />
-          </div>
+        <Link to="/dashboard" className={`${S.sidebarPage} ${getLinkClass('/dashboard')}`}>
+          <div className={S.icon}><FaTachometerAlt /></div>
           <p className={S.pageName}>Dashboard</p>
         </Link>
 
-        <Link
-          to="/account"
-          className={`${S.sidebarPage} ${getLinkClass("/account")}`}
-        >
-          <div className={S.icon}>
-            <FaWallet />
-          </div>
+        <Link to="/account" className={`${S.sidebarPage} ${getLinkClass('/account')}`}>
+          <div className={S.icon}><FaWallet /></div>
           <p className={S.pageName}>Account</p>
         </Link>
 
-        <Link
-          to="/transfer"
-          className={`${S.sidebarPage} ${getLinkClass("/transfer")}`}
-        >
-          <div className={S.icon}>
-            <FaExchangeAlt />
-          </div>
+        <Link to="/transfer" className={`${S.sidebarPage} ${getLinkClass('/transfer')}`}>
+          <div className={S.icon}><FaExchangeAlt /></div>
           <p className={S.pageName}>Transfer</p>
         </Link>
 
-        <Link
-          to="/payment"
-          className={`${S.sidebarPage} ${getLinkClass("/payment")}`}
-        >
-          <div className={S.icon}>
-            <FaCreditCard />
-          </div>
+        <Link to="/payment" className={`${S.sidebarPage} ${getLinkClass('/payment')}`}>
+          <div className={S.icon}><FaCreditCard /></div>
           <p className={S.pageName}>Payment</p>
         </Link>
 
-        <Link
-          to="/investment"
-          className={`${S.sidebarPage} ${getLinkClass("/investment")}`}
-        >
-          <div className={S.icon}>
-            <FaChartLine />
-          </div>
+        <Link to="/investment" className={`${S.sidebarPage} ${getLinkClass('/investment')}`}>
+          <div className={S.icon}><FaChartLine /></div>
           <p className={S.pageName}>Investment</p>
         </Link>
 
-        <Link
-          to="/loan"
-          className={`${S.sidebarPage} ${getLinkClass("/loan")}`}
-        >
-          <div className={S.icon}>
-            <FaHandHoldingUsd />
-          </div>
+        <Link to="/loan" className={`${S.sidebarPage} ${getLinkClass('/loan')}`}>
+          <div className={S.icon}><FaHandHoldingUsd /></div>
           <p className={S.pageName}>Loan</p>
         </Link>
       </div>
 
-      <Link
-        to="/settings"
-        className={`${S.sidebarPage} ${getLinkClass("/settings")}`}
-      >
-        <div className={S.icon}>
-          <FaCog />
-        </div>
+      <Link to="/settings" className={`${S.sidebarPage} ${getLinkClass('/settings')}`}>
+        <div className={S.icon}><FaCog /></div>
         <p className={S.pageName}>Settings</p>
       </Link>
 
       {isLogged && (
-        <Link
-          to="/"
-          onClick={handleLogout}
-          className={`${S.sidebarPage} ${getLinkClass("/logout")}`}
-        >
-          <div className={S.icon}>
-            <FaSignOutAlt />
-          </div>
+        <Link to="/" onClick={handleLogout} className={`${S.sidebarPage} ${getLinkClass('/logout')}`}>
+          <div className={S.icon}><FaSignOutAlt /></div>
           <p className={S.pageName}>Logout</p>
         </Link>
       )}
